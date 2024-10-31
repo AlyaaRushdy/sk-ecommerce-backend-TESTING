f439156c191c7ee1dfd1dafadf4423eddf2be6b0
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-31 18:52:12 +0300

    made some changes in product schema and in product index, store and update functions

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,22 @@
 const Product = require("../models/product");
 const mongoose = require("mongoose");
 
+const priceAfterDiscount = (originalPrice, discount) => {
+  if (discount) {
+    const discountedAmount = (originalPrice * discount) / 100;
+    return originalPrice - discountedAmount;
+  }
+  return originalPrice;
+};
+
+const stockStatus = (quantity) => {
+  if (quantity == 0) {
+    return "out-of-stock";
+  } else if (quantity <= 10) {
+    return "low-stock";
+  } else return "in-stock";
+};
+
 function store(req, res) {
   const imagePaths = req.files ? req.files.map((file) => file.path) : []; // get paths of all uploaded images
 
@@ -11,15 +27,14 @@ function store(req, res) {
     price: {
       base: req.body.base,
       discount: req.body.discount,
+      afterDiscount: priceAfterDiscount(req.body.base, req.body.discount),
     },
     description: req.body.description,
     categoryId: req.body.categoryId,
-    quantity: req.body.quantity,
+    stock: req.body.stock,
     scent: req.body.scent,
-    volume:req.body.volume,
-    rating: req.body.rating,
-    status: req.body.status,
-})
+    volume: req.body.volume,
+  });
   product
     .save()
     .then((product) => {
@@ -37,54 +52,73 @@ function store(req, res) {
     });
 }
 
-function update(req, res) {
+async function update(req, res) {
   const id = req.params.id;
+  const imagePaths = req.files ? req.files.map((file) => file.path) : []; // get paths of all uploaded images
 
-  const updatedData = {
-    sku: req.body.sku,
-    title: req.body.title,
-    price: {
-      base: req.body.base,
-      discount: req.body.discount,
-    },
-    description: req.body.description,
-    images: req.files ? req.files.map((file) => file.path) : undefined, // Handle multiple images
-    categoryId: req.body.categoryId || null,
-    quantity: req.body.quantity,
-    scent: req.body.scent,
-    volume:req.body.volume,
-    rating: req.body.rating,
-    status: req.body.status,
-  };
-
-  Product.findByIdAndUpdate(id, updatedData, { new: true })
-    .then((product) => {
-      if (product.length === 0) {
-        return res.status(404).json({
-          message: "Product Not Found",
-        });
-      }
-      res.status(200).json({
-        message: "Product Updated Successfully",
-        product: product,
-      });
-    })
+  const product = await Product.findById(id)
+    .then(res)
     .catch((err) => {
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ message: "Invalid Product ID" });
-      } else {
-        res.status(500).json({
-          error: err,
-        });
-      }
+      return res.status(500).json({
+        error: err,
+      });
     });
+
+  if (product) {
+    const afterDiscount = () => {
+      if (!req.body.base && !req.body.discount)
+        return product.price.afterDiscount;
+      else if (req.body.base && !req.body.discount)
+        return priceAfterDiscount(req.body.base, product.price.discount);
+      else if (!req.body.base && req.body.discount)
+        return priceAfterDiscount(product.price.base, req.body.discount);
+      else return priceAfterDiscount(req.body.base, req.body.discount);
+    };
+
+    const updatedData = {
+      images: imagePaths,
+      sku: req.body.sku,
+      title: req.body.title,
+      price: {
+        base: req.body.base || product.price.base,
+        discount: req.body.discount || product.price.discount,
+        afterDiscount: afterDiscount(),
+      },
+      description: req.body.description,
+      categoryId: req.body.categoryId,
+      stock: req.body.stock || product.stock,
+      scent: req.body.scent,
+      volume: req.body.volume,
+      status: stockStatus(req.body.stock),
+    };
+
+    Product.updateOne({ _id: id }, updatedData)
+      .then(() => {
+        res.status(200).json({
+          message: "Product Updated Successfully",
+        });
+      })
+      .catch((err) => {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          return res.status(400).json({ message: "Invalid Product ID" });
+        } else {
+          res.status(500).json({
+            error: err,
+          });
+        }
+      });
+  } else {
+    return res.status(404).json({
+      message: "product not found",
+    });
+  }
 }
 
 function show(req, res) {
   const id = req.params.id;
   Product.findById(id)
     .then((product) => {
-      if (product.length === 0) {
+      if (!product) {
         return res.status(404).json({
           message: "Product Not Found",
         });
@@ -107,7 +141,7 @@ function show(req, res) {
 
 function index(req, res) {
   Product.find({})
-    // .select('title _id images price rating')
+    .populate("categoryId", "categoryTitle _id")
     .then((products) => {
       if (products.length === 0) {
         res.status(404).json({
@@ -124,14 +158,20 @@ function index(req, res) {
           products: products.map((product) => {
             return {
               id: product._id,
+              sku: product.sku,
               title: product.title,
               basePrice: product.price.base,
+              priceAfterDiscount: product.price.afterDiscount,
               discountPercentage: product.price.discount,
-              rating: product.rating,
-              quantity: product.quantity,
+              image: product.images[0],
+              stock: product.stock,
+              status: product.status,
+              categoryId: product.categoryId._id,
+              categoryTitle: product.categoryId.categoryTitle,
               scent: product.scent,
               volume: product.volume,
-              rating: product.rating
+              rating: product.rating,
+              createdAt: product.createdAt,
             };
           }),
         });
--- models/product.js
+++ models/product.js
@@ -1,26 +1,40 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var products = new Schema(
+var productsSchema = new Schema(
   {
     sku: { type: String, required: true },
     title: { type: String, required: true },
     price: {
       base: { type: Number, required: true },
       discount: { type: Number, default: 0 },
+      afterDiscount: { type: Number, required: false },
     },
-    description: { type: String, required:false },
-    images: [ {type: String , required: false, }, ],
-    categoryId: { type: Schema.Types.ObjectId, ref:"category", required: false },
-    quantity: { type: Number, required: true, default: 1},
-    scent :{ type:String,  required:false },
-    volume :{type:String , required:false},
-    gender:{type:String ,required:false},
-    rating: { type: Number,default: 0 },
-    status: { type: String, required: true},
-},
-  
+    description: { type: String, required: false },
+    images: [{ type: String, required: false }],
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: false,
+    },
+    stock: { type: Number, required: true },
+    scent: { type: String, required: false },
+    volume: { type: String, required: false },
+    gender: { type: String, required: false },
+    rating: { type: Number, default: 0 },
+    status: { type: String, default: "in-stock" },
+  },
+
   { timestamps: true }
 );
 
-module.exports = mongoose.model('Product', products);
+productsSchema.pre(["find", "findById", "save"], function (next) {
+  if (this.stock == 0) {
+    this.status = "out-of-stock";
+  } else if (this.stock <= 10) {
+    this.status = "low-stock";
+  }
+  next();
+});
+
+module.exports = mongoose.model("Product", productsSchema);
